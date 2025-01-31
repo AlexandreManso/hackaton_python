00b146498a58a72da14f52bd49f914e5ce7ed1e7
AntAmeline <antonin.ameline@etu.minesparis.psl.eu>
2025-01-31 14:49:10 +0100

    Added command line arguments and support.

--- argument.py
+++ argument.py
@@ -5,22 +5,34 @@ from pathlib import Path
 
 #Global constants
 DEFAULT_MAPPING = {"(1,1)" : Path("mappings/mapping1.yml")}
-# Global constants
-DEFAULT_MAPPING = Path("mapping.yml")
-DEFAULT_
+DEFAULT_HEIGHT = 800
+DEFAULT_WIDTH = 1200
+DEFAULT_TILESIZE = 40
+
 
 
 def read_args() -> argparse.Namespace:
     """Read command line arguments."""
+
     parser = argparse.ArgumentParser(
         description = "Rogue-like game.",
         formatter_class = argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("--MAPPING1", "-MP1"
-                        help= ".yaml file for the screens.",
+    parser.add_argument("--MAPPING", "-MP",
+                        help= "Dictionnary with screens.",
                         default= DEFAULT_MAPPING)
-    
-    
-     
-
+    parser.add_argument("--HEIGHT", "-ht",
+                        help = "Height of the screen.",
+                        default = DEFAULT_HEIGHT,
+                        type = int)
+    parser.add_argument("--WIDTH", "-wd",
+                        help = "Width of the screen.",
+                        default = DEFAULT_WIDTH,
+                        type = int)
+    parser.add_argument("--TILESIZE", "-ts",
+                        help = "Size for a tile.",
+                        default = DEFAULT_TILESIZE,
+                        type= int)
     args = parser.parse_args()
+    return args
+
     
